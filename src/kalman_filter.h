c2fc5b45c9866434eaec34cf2e888cfce2182c88
Scott Henderson <henders999@gmail.com>
2018-10-22 10:13:36 -0700

    Added UpdateHelper() for common Update*() code.

--- src/kalman_filter.h
+++ src/kalman_filter.h
@@ -64,6 +64,10 @@ public:
    */
   void UpdateEKF(const Eigen::VectorXd &z);
 
+  /**
+   * Update helper function for common code.
+   */
+  void UpdateHelper(const Eigen::VectorXd &y);
 };
 
 #endif /* KALMAN_FILTER_H_ */
